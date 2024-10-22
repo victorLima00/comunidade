8f3a8fb9f9a7890d27397a2f92710f24fa82bcea
victorLima00 <nascimentolima.victor@gmail.com>
2024-10-22 10:34:13 -0300

    projeto inicial

---
+++ src/test/java/com/api/comunidade/ComunidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.comunidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
